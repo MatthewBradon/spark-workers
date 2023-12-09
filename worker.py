bf90c781e4614aad8dfc6180bdfafb6a171ff681
Matthew Bradon <matthewbradon@gmail.com>
2023-12-09 13:27:32 +0000

    uppdated secret resource name to latest 2

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def get_api_key() -> str:
     client = secretmanager.SecretManagerServiceClient()
-    secret_version_name = "projects/737526740663/secrets/compute-api-key/versions/latest"
+    secret_version_name = "projects/737526740663/secrets/compute-api-key/latest"
     try:
         # Access the secret version
         response = client.access_secret_version(name=secret_version_name)
